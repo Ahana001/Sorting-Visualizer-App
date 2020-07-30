c8b7043a06dd1b09a3160386649c8c068c6ee186
Ankita <thakkarankita0105@.github.com>
2020-07-30 20:50:01 +0530

    flutter_bezier_app
    In this repo you will see how to add Bezier Curve using Clipping.

---
+++ beizer_curve_ui/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:beizer_curve_ui/Pages/Page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(body: Container(child: Page())));
+  }
+}
